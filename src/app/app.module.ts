7191b1b757085125d0374ccd0e9ddf83fe6cb953
Prabesh <iprabesh@hotmail.com>
2018-09-17 10:47:20 -0500

    Added Best Seller Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { BestSellerComponent } from './best-seller/best-seller.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    BestSellerComponent
   ],
   imports: [
     BrowserModule
